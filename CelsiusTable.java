8277f794af5c749b4c8a58470917cfe3afedce43
Jac123-glitch <moreno.jacqueline@student.ccm.edu>
2026-09-10 13:59:34 -0400

    Week2-lab1-Celsius

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,17 @@
+import java.util.*;
 public class CelsiusTable {
+    public static double celsius(double fahrenheit){
+        return (5.0/9.0) * (fahrenheit -32);
+    }
+
+    public static void main(String [] args){
+        System.out.println("Fahrenheit\tCelsius");
+        System.out.println("---------------------");
+
+        for(int f = 0; f <=20; f++){
+            double c = celsius(f);
+            System.out.printf("%d\t\t%.2f\n",f,c);
+        }
+    }
     
 }
